45208319d8c350b60bd37540fa0896793c92bdf9
laianribas <laian67a@gmail.com>
2022-01-06 12:23:25 -0300

    layout pronto

---
+++ src/Main/Calculator.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import Button from '../components/Button'
+import Display from '../components/Display'
+import './Calculator.css'
+
+const Calculator = props => {
+  const clearMemory = () => {
+    console.log('limpar')
+  }
+
+  const setOperation = operation => {
+    console.log(operation)
+  }
+
+  const addDigit = n => {
+    console.log(n)
+  }
+
+  return (
+    <div className="Calculator">
+      <Display value={100} />
+      <Button label="AC" click={clearMemory} triple />
+      <Button label="/" click={setOperation} operation={true} />
+      <Button label="7" click={addDigit} />
+      <Button label="8" click={addDigit} />
+      <Button label="9" click={addDigit} />
+      <Button label="*" click={setOperation} operation />
+      <Button label="4" click={addDigit} />
+      <Button label="5" click={addDigit} />
+      <Button label="6" click={addDigit} />
+      <Button label="-" click={setOperation} operation />
+      <Button label="1" click={addDigit} />
+      <Button label="2" click={addDigit} />
+      <Button label="3" click={addDigit} />
+      <Button label="+" click={setOperation} operation />
+      <Button label="0" click={addDigit} double />
+      <Button label="." click={addDigit} />
+      <Button label="=" click={setOperation} operation />
+    </div>
+  )
+}
+
+export default Calculator
